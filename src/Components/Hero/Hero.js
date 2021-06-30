ba043cf99bfed737bc88431e03d4c0e18c98989a
David Fertitta <davidfertitta@Davids-MacBook-Pro-2.local>
2021-06-29 22:28:22 -0600

    home page with some responsivity

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import CPU from '../../Images/flippedcpu.png';
+import CPU2 from '../../Images/cpu.png';
 
 import './Hero.css';
 import '../../App.css';
@@ -7,7 +9,24 @@ const Hero = () => {
   return (
     <div className='hero-container'>
       <div className='two-side-container'>
-        <div className='left-side'></div>
+        <div className='left-side'>
+          <div className='mobile-titles-container'>
+            <p className='mobile-intro'>HI, I'M</p>
+            <h1 className='mobile-name'>DAVID FERTITTA</h1>
+            <h3 className='mobile-description'>DEVELOPER AND MORE</h3>
+          </div>
+          <div className='image-container'>
+            <img className='cpu' src={CPU} />
+          </div>
+          <div className='mobile-image-container'>
+            <div>
+              <img className='mobile-cpu' src={CPU} />
+            </div>
+            <div>
+              <img className='cpu2' src={CPU2} />
+            </div>
+          </div>
+        </div>
         <div className='titles-container'>
           <p className='intro'>HI, I'M</p>
           <h1 className='name'>DAVID FERTITTA</h1>
