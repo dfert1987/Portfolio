26adf7d0bfaf274b4dd1ca8929dd099df9cee5e4
David Fertitta <davidfertitta@Davids-MacBook-Pro-2.local>
2021-07-01 15:15:30 -0600

    added  links to all items in masonry

--- src/Components/Masonry/Masonry.js
+++ src/Components/Masonry/Masonry.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Floppy from '../../Images/floppy.png';
+import Link from 'react-router-dom';
 import './Masonry.css';
 
 const Masonry = () => {
@@ -10,24 +11,36 @@ const Masonry = () => {
         <div className='masonry-plus-images'>
           <img className='floppy1' src={Floppy} />
           <div className='masonry-container'>
-            <div className='card about'>
-              <h3 className='about-text'>ABOUT</h3>
-            </div>
-            <div className='card resume'>
-              <h3 className='resume-text'>RESUME</h3>
-            </div>
-            <div className='card projects'>
-              <h3 className='projects-text'>PROJECTS</h3>
-            </div>
-            <div className='card blogs'>
-              <h3 className='blogs-text'>BLOGS</h3>
-            </div>
-            <div className='card design'>
-              <h3 className='design-text'>DESIGN</h3>
-            </div>
-            <div className='card contact'>
-              <h3 className='contact-text'>CONTACT</h3>
-            </div>
+            <Link to='/about'>
+              <div className='card about'>
+                <h3 className='about-text'>ABOUT</h3>
+              </div>
+            </Link>
+            <Link to='/resume'>
+              <div className='card resume'>
+                <h3 className='resume-text'>RESUME</h3>
+              </div>
+            </Link>
+            <Link to='/projects'>
+              <div className='card projects'>
+                <h3 className='projects-text'>PROJECTS</h3>
+              </div>
+            </Link>
+            <Link to='/blogs'>
+              <div className='card blogs'>
+                <h3 className='blogs-text'>BLOGS</h3>
+              </div>
+            </Link>
+            <Link to='/design'>
+              <div className='card design'>
+                <h3 className='design-text'>DESIGN</h3>
+              </div>
+            </Link>
+            <Link to='/contact'>
+              <div className='card contact'>
+                <h3 className='contact-text'>CONTACT</h3>
+              </div>
+            </Link>
           </div>
           <img className='floppy' src={Floppy} />
         </div>
