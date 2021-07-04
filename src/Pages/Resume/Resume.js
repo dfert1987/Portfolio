cdf8f06b988be37b625ca99a080683eb1f6b72b5
David Fertitta <davidj.fertitta@gmail.com>
2021-07-04 12:34:16 -0500

    pdf added

--- src/Pages/Resume/Resume.js
+++ src/Pages/Resume/Resume.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import NavBar from '../../Components/Nav/NavBar';
+import Button from '../../Components/Button/Button';
+import Res from '../../Images/resume.pdf';
 import './Resume.css';
 
 const Resume = () => {
@@ -7,9 +9,24 @@ const Resume = () => {
     <>
       <NavBar />
       <div className='resume-container'>
-        <div className='rsume-title-container'>
+        <div className='resume-title-container'>
           <div className='rectangle'>
             <h1 className='resume-title'>RESUME</h1>
+          </div>
+        </div>
+        <div className='main-container'>
+          <div className='download-title-container'>
+            <h2 className='download-title'>View or Download Resume Below</h2>
+          </div>
+          <div className='pdf-container'>
+            <embed
+              src={Res}
+              width='80%'
+              height='90%'
+              frameBorder='2px'
+              type='application/pdf'
+              className='pdf'
+            ></embed>
           </div>
         </div>
       </div>
