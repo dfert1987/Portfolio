aa9221dd6a82b83e5c36fbe416d10234216ba34e
David Fertitta <davidj.fertitta@gmail.com>
2021-07-08 10:40:03 -0600

    all projects and details added. no main pics yet

--- src/Pages/Projects/Projects.js
+++ src/Pages/Projects/Projects.js
@@ -4,6 +4,10 @@ import {Button} from '../../Components/Button/Button';
 import fakeImage from '../../Images/mepork.jpeg';
 import reactLogo from '../../Images/react.png';
 import railsLogo from '../../Images/rails.png';
+import typescriptLogo from '../../Images/typescript.png';
+import nodeLogo from '../../Images/node.png';
+import jsLogo from '../../Images/js.png';
+import nativeLogo from '../../Images/reactnative.png';
 import './Projects.css';
 
 const Projects = () => {
@@ -56,14 +60,47 @@ const Projects = () => {
             </div>
             <div className='details'>
               <div className='center'>
-                <h1 className='project-name'>HOMER</h1>
-                <h3 className='subtitle'>A Sports Blog</h3>
+                <h1 className='project-name'>NeuroCann Login</h1>
+                <h3 className='subtitle'>A Cannabis ERP</h3>
                 <p className='project-description'>
-                  HOMER is a Chicago sports themed sports blog in the style of
-                  something you may make with Wordpress. However, HOMER was made
-                  from scratch and is complete with full auth, admin privelages,
-                  and a rich text editor. This one is a work in process, but
-                  will hopefully be finished in the near future.
+                  NeuroCann is a cannabis erp. During my time there, I did a lot
+                  of front end work including, interactive tables, modals, and
+                  this sleek login page featuring responsive design, and an
+                  elegant switch statement to toggle between the views.
+                </p>
+                <div className='stack-container'>
+                  <h3 className='stack-title'>Stack:</h3>
+                  <div className='logos'>
+                    <img className='logo1' src={reactLogo} />
+                    <img className='logo2' src={nodeLogo} />
+                    <img className='logo3' src={typescriptLogo} />
+                  </div>
+                </div>
+                <div className='button-container'>
+                  <Button buttonStyle='btn--primary' className='demo-button'>
+                    VIEW DEMO
+                  </Button>
+                  <Button buttonStyle='btn--outline' className='demo-button'>
+                    VISIT REPO
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='card-wrapper'>
+            <div className='image-container'>
+              <img className='image' src={fakeImage} />
+            </div>
+            <div className='details'>
+              <div className='center'>
+                <h1 className='project-name'>Angler</h1>
+                <h3 className='subtitle'>A Fishing Log Book</h3>
+                <p className='project-description'>
+                  Angler allows users to log their fishing trips to improve
+                  future performance. Through the Google Maps API, users can
+                  mark fishing spots, and log various trip details. The app also
+                  allows to catalog species of fish caught, as well as organize
+                  their own personal tacklebox.
                 </p>
                 <div className='button-container'>
                   <Button buttonStyle='btn--primary' className='demo-button'>
@@ -77,6 +114,70 @@ const Projects = () => {
                   <h3 className='stack-title'>Stack:</h3>
                   <div className='logos'>
                     <img className='logo1' src={reactLogo} />
+                    <img className='logo2' src={railsLogo} />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='card-wrapper'>
+            <div className='image-container'>
+              <img className='image' src={fakeImage} />
+            </div>
+            <div className='details'>
+              <div className='center'>
+                <h1 className='project-name'>Global Crate Digger</h1>
+                <h3 className='subtitle'>World Music Discovery App</h3>
+                <p className='project-description'>
+                  An early project built in Vanilla JS that allows users to
+                  discover and listen to new music from around the globe by
+                  interacting with sleek interactive maps!
+                </p>
+                <div className='button-container'>
+                  <Button buttonStyle='btn--primary' className='demo-button'>
+                    VIEW DEMO
+                  </Button>
+                  <Button buttonStyle='btn--outline' className='demo-button'>
+                    VISIT REPO
+                  </Button>
+                </div>
+                <div className='stack-container'>
+                  <h3 className='stack-title'>Stack:</h3>
+                  <div className='logos'>
+                    <img className='logo1' src={jsLogo} />
+                    <img className='logo2' src={railsLogo} />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='card-wrapper'>
+            <div className='image-container'>
+              <img className='image' src={fakeImage} />
+            </div>
+            <div className='details'>
+              <div className='center'>
+                <h1 className='project-name'>Big League Eats</h1>
+                <h3 className='subtitle'>Mobile Stadium Food Review App</h3>
+                <p className='project-description'>
+                  Dabbling in React Native, I started a project that allowed
+                  users to review food vendors/stalls at MLB stadiums based on
+                  their seat section. Users can add vendors and review existing
+                  vendors. (This is a very rough start, to an idea. Would love
+                  to flush this out and fix it up in the future.)
+                </p>
+                <div className='button-container'>
+                  <Button buttonStyle='btn--primary' className='demo-button'>
+                    VIEW DEMO
+                  </Button>
+                  <Button buttonStyle='btn--outline' className='demo-button'>
+                    VISIT REPO
+                  </Button>
+                </div>
+                <div className='stack-container'>
+                  <h3 className='stack-title'>Stack:</h3>
+                  <div className='logos'>
+                    <img className='logo1' src={nativeLogo} />
                     <img className='logo2' src={railsLogo} />
                   </div>
                 </div>
