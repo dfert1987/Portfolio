34e539d88e396f5d463fd89b1a31cb6ece2d6ffe
David Fertitta <davidj.fertitta@gmail.com>
2021-07-09 14:50:48 -0600

    html for one blog

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import NavBar from '../../Components/Nav/NavBar';
+import {Button} from '../../Components/Button/Button';
 
 import './Blogs.css';
 
@@ -11,6 +12,35 @@ const Blogs = () => {
         <div className='blogs-title-container'>
           <div className='rectangle'>
             <h1 className='blogs-title'>BLOGS</h1>
+          </div>
+        </div>
+        <div className='cards-container'>
+          <div className='blog-card'>
+            <div className='blog-card-background'>
+              <div className='blog-card-background-wrapper'>
+                <div className='blog-card-background-main'>
+                  <div className='blog-card-background-layer'></div>
+                </div>
+              </div>
+            </div>
+            <div className='blog-card-head'>
+              <span className='date-box'>
+                <span className='date-day'>11</span>
+                <span className='date_month'>JAN</span>
+              </span>
+            </div>
+            <div className='blog-card-info'>
+              <h3>RICH TEXT EDITORS: QUILL JS</h3>
+              <p>
+                blog preafdafdaslfkj asdlkfj lasdjfldasjfasdfasdfl;kdasjfljadsf
+                jsdlkfjldskj
+              </p>
+              <a href='/'>
+                  <Button buttonStyle='btn--primary' className='blog-button'>
+                      READ MORE
+                  </Button>
+              </a>
+            </div>
           </div>
         </div>
       </div>
