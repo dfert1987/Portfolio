f98a14ca9ba2791d4b506e1e04735e01ffc7ed5c
David Fertitta <davidj.fertitta@gmail.com>
2021-07-12 19:47:58 -0600

    carousel added

--- src/Pages/Design/Design.js
+++ src/Pages/Design/Design.js
@@ -7,11 +7,26 @@ const Design = () => {
     <>
       <NavBar />
       <div className='design-container'>
-          <div className='design-title-container'>
-              <div className='rectangle'>
-                  <h1 className='design-title'>DESIGN</h1>
-              </div>
+        <div className='design-title-container'>
+          <div className='rectangle'>
+            <h1 className='design-title'>DESIGN</h1>
           </div>
+        </div>
+        <h2 className='posters'>Event Poster Examples</h2>
+        <div className='carousel-container'>
+          <input type='radio' name='position' />
+          <input type='radio' name='position' />
+          <input type='radio' name='position' />
+          <input type='radio' name='position' />
+          <input type='radio' name='position' />
+          <main id='carousel'>
+            <div class='item'></div>
+            <div class='item'></div>
+            <div class='item'></div>
+            <div class='item'></div>
+            <div class='item'></div>
+          </main>
+        </div>
       </div>
     </>
   );
