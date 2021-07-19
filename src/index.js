5f940cf996128292e46700c2c42f90126689c4e3
David Fertitta <davidj.fertitta@gmail.com>
2021-07-19 16:41:41 -0600

    font trial

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import {BrowserRouter as Router} from 'react-router-dom';
+import '@fontsource/lato';
+import '@fontsource/league-gothic';
 
 ReactDOM.render(
   <React.StrictMode>
