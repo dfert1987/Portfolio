213e21486f1b2855459517d5df4084ec87c7bc0b
David Fertitta <davidj.fertitta@gmail.com>
2021-07-19 09:49:24 -0600

    transitions added for home titles on desktop and mobile

--- src/Animations/index.js
+++ src/Animations/index.js
@@ -7,6 +7,37 @@ export const animationOne = {
     },
   };
 
+export const animationTwo = {
+  in: {
+    opacity: 1,
+    x: 0,
+  },
+  out: {
+    opacity: 0,
+    x: 300,
+  },
+  end: {
+    x: 0,
+    opacity: 1,
+  },
+}
+
+export const animationThree = {
+  in: {
+    opacity: 1,
+    x: 0,
+  },
+  out: {
+    opacity: 0,
+    x: -300,
+  },
+  end: {
+    x: 0,
+    opacity: 1,
+  },
+}
+
+
   export const transition = {
     duration: 1
   };
--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -2,7 +2,7 @@ import React from 'react';
 import CPU from '../../Images/flippedcpu.png';
 import CPU2 from '../../Images/cpu.png';
 import {motion} from 'framer-motion';
-import {animationOne, transition} from '../../Animations';
+import {animationTwo, animationThree, transition} from '../../Animations';
 import './Hero.css';
 import '../../App.css';
 
@@ -14,15 +14,31 @@ const Hero = () => {
           <div className='mobile-titles-container'>
             <motion.p
               initial='out'
-              animate='in'
+              animate='end'
               transition={transition}
-              variants={animationOne}
+              variants={animationTwo}
               className='mobile-intro'
             >
               HI, I'M
             </motion.p>
-            <h1 className='mobile-name'>DAVID FERTITTA</h1>
-            <h3 className='mobile-description'>FULL STACK DEV</h3>
+            <motion.h1
+              initial='out'
+              animate='end'
+              transition={transition}
+              variants={animationThree}
+              className='mobile-name'
+            >
+              DAVID FERTITTA
+            </motion.h1>
+            <motion.h3
+              className='mobile-description'
+              initial='out'
+              animate='end'
+              transition={transition}
+              variants={animationTwo}
+            >
+              FULL STACK DEV
+            </motion.h3>
           </div>
           <div className='image-container'>
             <img className='cpu' src={CPU} alt='old computer' />
@@ -41,13 +57,29 @@ const Hero = () => {
             initial='out'
             animate='in'
             transition={transition}
-            variants={animationOne}
+            variants={animationTwo}
             className='intro'
           >
             HI, I'M
           </motion.p>
-          <h1 className='name'>DAVID FERTITTA</h1>
-          <h3 className='description'>FULL STACK DEV</h3>
+          <motion.h1
+            className='name'
+            initial='out'
+            animate='end'
+            transition={transition}
+            variants={animationThree}
+          >
+            DAVID FERTITTA
+          </motion.h1>
+          <motion.h3
+            className='description'
+            initial='out'
+            animate='end'
+            transition={transition}
+            variants={animationTwo}
+          >
+            FULL STACK DEV
+          </motion.h3>
         </div>
         <div className='right-side'></div>
       </div>
